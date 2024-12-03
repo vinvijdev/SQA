3ef4f9e46ccd29e667f6960d8ba34878614d9767
Vineet Vijayan <vineet.vijayan@sap.com>
2024-12-03 09:21:29 +0530

    downgraded android core ktx version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
 
                     groupId = "com.github.vinvijdev"
                     artifactId = "SQA"
-                    version = "1.0.27"
+                    version = "1.0.28"
                 }
             }
         }
@@ -80,7 +80,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
